8934546abd7e0eb10235e3546ee3c32cc4681698
Blaise <blaise.mahamoodally@gmail.com>
2026-04-05 00:47:03 -0400

    modified attributes

--- app/src/main/java/com/ehotels/app/model/Renting.java
+++ app/src/main/java/com/ehotels/app/model/Renting.java
@@ -2,9 +2,13 @@ package com.ehotels.app.model;
 import java.time.LocalDate;
 
 public record Renting(
-    int id, 
+    Integer id,
+    Integer roomNumber, 
+    String hotelAddress, 
+    String chainName, 
+    Integer customerId, 
     LocalDate startDate, 
     LocalDate endDate, 
     LocalDate registrationDate, 
-    String payment
+    Integer payment
 ){}
