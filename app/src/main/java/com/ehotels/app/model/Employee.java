a6fee00c216d0c93c23c8799f311227bc08a9269
Blaise <blaise.mahamoodally@gmail.com>
2026-04-04 20:49:28 -0400

    changed type + added transport field for role

--- app/src/main/java/com/ehotels/app/model/Employee.java
+++ app/src/main/java/com/ehotels/app/model/Employee.java
@@ -2,11 +2,12 @@ package com.ehotels.app.model;
 import java.time.LocalDate;
 
 public record Employee(
-    int ssn, 
+    Integer ssn, 
     String chainName, 
     String hotelAddress, 
     String firstName, 
     String lastName, 
     String address, 
-    LocalDate registrationDate
+    LocalDate registrationDate,
+    String role
 ){}
